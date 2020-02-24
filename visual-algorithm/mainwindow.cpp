de0fa159e43adde10d94d6f396eebec417c81d3e
cassuto <diyer175@hotmail.com>
2020-02-25 07:55:09 +0800

    Updating gitignore.

--- visual-algorithm/mainwindow.cpp
+++ visual-algorithm/mainwindow.cpp
@@ -1,8 +1,11 @@
 #include "mainwindow.h"
+#include <QMdiArea>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+      m_mdiArea(new QMdiArea(this))
 {
+    this->setCentralWidget(m_mdiArea);
 }
 
 MainWindow::~MainWindow()
--- visual-algorithm/mainwindow.h
+++ visual-algorithm/mainwindow.h
@@ -10,6 +10,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+protected:
+    class QMdiArea *m_mdiArea;
 };
 
 #endif // MAINWINDOW_H
