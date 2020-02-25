f3f8c3560e47a223dcc5379dc6739412472a5338
cassuto <diyer175@hotmail.com>
2020-02-25 08:42:48 +0800

    Updating algomenu: QListWidget to show options.

--- visual-algorithm/formalgorithmmenu.cpp
+++ visual-algorithm/formalgorithmmenu.cpp
@@ -1,22 +1,31 @@
 #include <QToolBox>
+#include <QListWidget>
+#include <QVBoxLayout>
 #include "formalgorithmmenu.h"
 
 FormAlgorithmMenu::FormAlgorithmMenu(QWidget *parent)
     : QWidget(parent),
       m_toolBox(new QToolBox(this))
 {
-    this->setFixedSize(QSize(200,200));
-    this->setWindowTitle("Select a Algorithm");
+    // 初始化垂直布局
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->setContentsMargins(0,0,0,0);
+    layout->setSpacing(0);
 
+    // 初始化抽屉式工具栏
     const int npages = 4;
-    QWidget *pages[npages];
+    QListWidget *pages[npages];
     const char *pagesText[npages] = {"贪心算法","分治算法","图论算法","递归算法"};
 
     for(int i=0;i<npages;++i) {
-        pages[i] = new QWidget(this);
+        pages[i] = new QListWidget(this);
     }
     for(int i=0;i<npages; ++i) {
         m_toolBox->addItem(pages[i], QIcon(":/new/icons/assets/algogroup.png"), pagesText[i]);
     }
+    layout->addWidget(m_toolBox);
+
+    // 初始化窗体栏
+    this->setWindowTitle("Select a Algorithm");
 }
 
